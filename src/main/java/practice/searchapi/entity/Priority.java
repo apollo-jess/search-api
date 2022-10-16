75af93772e54a1f0cf25e9b3097b1a1a3dcdea76
apollo-jess <jesskim1126@gmail.com>
2022-10-17 03:00:32 +0900

    feat add Priority

---
+++ src/main/java/practice/searchapi/entity/Priority.java
@@ -0,0 +1,49 @@
+package practice.searchapi.entity;
+
+import java.util.Objects;
+
+public class Priority {
+
+    private final API api;
+
+    private final boolean duplicated;
+
+    public Priority(API api) {
+        this.api = api;
+        this.duplicated = false;
+    }
+
+    public Priority(Priority priority, boolean duplicated) {
+        this.api = priority.getApi();
+        this.duplicated = duplicated;
+    }
+
+    public API getApi() {
+        return api;
+    }
+
+    public boolean isDuplicated() {
+        return duplicated;
+    }
+
+    @Override
+    public String toString() {
+        return "Priority{" +
+                "api=" + api +
+                ", duplicated=" + duplicated +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Priority priority = (Priority) o;
+        return duplicated == priority.duplicated && api == priority.api;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(api, duplicated);
+    }
+}
