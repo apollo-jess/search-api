22f7154185084f664ac015b4793f0c68063f9504
apollo-jess <jesskim1126@gmail.com>
2022-10-17 02:59:34 +0900

    feat add Enum API

---
+++ src/main/java/practice/searchapi/entity/API.java
@@ -0,0 +1,15 @@
+package practice.searchapi.entity;
+
+public enum API {
+    KAKAO("kakao", 1),
+    NAVER("naver", 2);
+
+    private final String type;
+
+    private final int order;
+
+    API(String type, int order) {
+        this.type = type;
+        this.order = order;
+    }
+}
