39d14c96384bf6616dbf92e4c461481e5ff07a77
apollo-jess <jesskim1126@gmail.com>
2022-10-18 23:08:06 +0900

    refactor compare (PlaceComparator)

--- src/main/java/practice/searchapi/util/PlaceComparator.java
+++ src/main/java/practice/searchapi/util/PlaceComparator.java
@@ -1,6 +1,5 @@
 package practice.searchapi.util;
 
-import practice.searchapi.entity.API;
 import practice.searchapi.entity.Place;
 import practice.searchapi.entity.Priority;
 
@@ -13,14 +12,6 @@ public class PlaceComparator implements Comparator<Place> {
         Priority priority1 = place1.getPriority();
         Priority priority2 = place2.getPriority();
 
-        if (priority1.isDuplicated() && !priority2.isDuplicated()) {
-            return -1;
-        }
-
-        if (priority1.getApi() == API.KAKAO && priority2.getApi() == API.NAVER) {
-            return -1;
-        }
-
-        return 0;
+        return priority1.isHigherPriorityThan(priority2);
     }
 }
