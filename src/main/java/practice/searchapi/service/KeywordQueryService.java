29391b3977e1d1e437ca7895e6817d1d8e8415eb
apollo-jess <jesskim1126@gmail.com>
2022-10-20 22:21:25 +0900

    feat add Transactional-readOnly for QueryService

--- src/main/java/practice/searchapi/service/KeywordQueryService.java
+++ src/main/java/practice/searchapi/service/KeywordQueryService.java
@@ -2,6 +2,7 @@ package practice.searchapi.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import practice.searchapi.entity.Keyword;
 import practice.searchapi.entity.Keywords;
 import practice.searchapi.entity.repository.KeywordRepository;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class KeywordQueryService {
 
     private final KeywordRepository keywordRepository;
